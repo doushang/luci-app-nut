dda97915fcaa61c0f0f00eb8e6b184874cbcac4d
sudodou <wsdosh@gmail.com>
2022-12-10 23:09:58 +0800

    Update build.sh

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -3,6 +3,7 @@
 sed -i 's/git\.openwrt\.org\/project\/luci/github\.com\/openwrt\/luci/g' ./feeds.conf.default
 ./scripts/feeds update luci
 ./scripts/feeds install luci
+rm -rf ./package/luci-app-nut
 mv ./bin/luci-app-nut ./package/
 make defconfig
 make package/luci-app-nut/compile V=s -j$(nproc) BUILD_LOG=1
